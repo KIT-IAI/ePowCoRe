31608365beb18f5ca192812ce2d7511f139e0233
Weinbrecht <ar4639@kit.edu>
2025-04-04 12:28:48 +0200

    Changed the code to use the original PFMOdel instead of the newer power_factory_model

--- epowcore/power_factory/from_gdf/power_factory_exporter.py
+++ epowcore/power_factory/from_gdf/power_factory_exporter.py
@@ -1,6 +1,5 @@
 from epowcore.gdf.core_model import CoreModel
-from epowcore.power_factory.power_factory_model import PowerFactoryModel
-
+from epowcore.power_factory.power_factory_converter import PFModel
 
 class PowerFactoryExporter:
     """Class holding the powerfactory application instance doing all the converting to then
@@ -9,5 +8,5 @@ class PowerFactoryExporter:
     
     
     
-    def export_power_factory(core_model: CoreModel, name: str) -> PowerFactoryModel:
+    def export_power_factory(core_model: CoreModel, name: str) -> PFModel:
         
