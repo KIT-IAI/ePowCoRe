aaab916e9e76125c08c29a693ac6f79c49878278
Johannes Weinbrecht <johannes.weinbrecht@kit.edu>
2025-04-09 16:45:50 +0200

    Removed unknown busType attribute and changed other attributes

--- epowcore/power_factory/from_gdf/components/bus.py
+++ epowcore/power_factory/from_gdf/components/bus.py
@@ -1,10 +1,10 @@
 import powerfactory as pf
 
-from epowcore.gdf.bus import Bus, BusType, LFBusType
-from epowcore.generic.logger import Logger
+from epowcore.gdf.bus import Bus, BusType #, LFBusType
+# from epowcore.generic.logger import Logger
 
 
-def create_bus(app: pf.Application, bus: Bus) -> bool:
+def create_bus(app: pf.Application , bus: Bus) -> bool:
     """Convert and add the given gdf core model bus to the given powerfactory network.
 
     :param app: Powerfactory app object to create a new object.
@@ -15,24 +15,29 @@ def create_bus(app: pf.Application, bus: Bus) -> bool:
     :rtype: bool
     """
     # Conversion of bus type
-    bus_type = {BusType.JUNCTION.value: 0, BusType.BUSBAR.value: 1, BusType.INTERNAL.value: 2}
-    lf_bus_type = {
-        LFBusType.PQ.value: 1,
-        LFBusType.PV.value: 2,
-        LFBusType.SL.value: 3,
+    bus_type = {
+        BusType.JUNCTION: 0,
+        BusType.BUSBAR: 1,
+        BusType.INTERNAL: 2
     }
-    if bus.lf_bus_type == "ISOLATED":
-        Logger.log_to_selected(
-            f"Bus {bus.name} can not be converted because the load flow type is ISOLATED"
-        )
-        return False
+    # lf_bus_type = {
+    #     LFBusType.PQ: 1,
+    #     LFBusType.PV: 2,
+    #     LFBusType.SL: 3,
+    # }
+    # if bus.lf_bus_type == "ISOLATED":
+    #     Logger.log_to_selected(
+    #         f"Bus {bus.name} can not be converted because the load flow type is ISOLATED"
+    #     )
+    #     return False
     # Create bus inside of network
     pf_bus = app.CreateObject("ElmTerm")
     # Set attributes for newly created bus
     pf_bus.SetAttribute("loc_name", bus.name)
-    pf_bus.SetAttribute("uknom", bus.nominal_voltage)  # maybe pf-bus.uknom = ...
+    pf_bus.SetAttribute("uknom", bus.nominal_voltage)
     pf_bus.SetAttribute("iUsage", bus_type[bus.bus_type])
-    pf_bus.SetAttribute("systype", "AC")
-    pf_bus.SetAttribute("busType", lf_bus_type[bus.lf_bus_type])
+    pf_bus.SetAttribute("systype", 0) # 0: AC
+    # pf_bus.SetAttribute("busType", lf_bus_type[bus.lf_bus_type]) Unknown name of attribute
+    # In import deriaved from the ElmTerm.GetBusType() function
 
     return True
