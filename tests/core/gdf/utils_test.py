8b4f496fdd27097a3884c22a21291cc686bb35f3
Johannes Weinbrecht <johannes.weinbrecht@kit.edu>
2026-07-26 16:09:15 +0200

    added additional tests of the sanity check

--- tests/core/gdf/utils_test.py
+++ tests/core/gdf/utils_test.py
@@ -1,11 +1,16 @@
+import json
+import os
 import pathlib
+import sys
 import unittest
+from zipfile import Path
 
 from helpers.gdf_component_creator import GdfTestComponentCreator
 
 from epowcore.gdf.bus import Bus, LFBusType
 from epowcore.gdf.core_model import CoreModel
 from epowcore.gdf.utils import get_connected_bus
+from epowcore.generic.manipulation.flatten import flatten
 
 PATH = pathlib.Path(__file__).parent.resolve()
 
@@ -32,6 +37,43 @@ class UtilsTest(unittest.TestCase):
 
         bus = get_connected_bus(core_model.graph, tline)
         self.assertIn(bus, (bus_a, bus_b))
+
+    def test_sanity_check_IEEE39(self) -> None:
+        PATH = pathlib.Path(__file__).parent.parent.resolve()
+
+        with open(
+            PATH.parent.parent / "tests/models/gdf/IEEE39_gdf.json", "r", encoding="utf-8"
+        ) as file:
+            data_str = file.read()
+            data = json.loads(data_str)
+            core_model = CoreModel.import_dict(data)
+            # flatten(core_model)  # if the mode lis flattened this also fails
+
+        assert core_model.sanity_check()
+
+    def test_sanity_check_IEEE39_flat(self) -> None:
+        PATH = pathlib.Path(__file__).parent.parent.resolve()
+
+        with open(
+            PATH.parent.parent / "tests/models/gdf/IEEE39-flat_gdf.json", "r", encoding="utf-8"
+        ) as file:
+            data_str = file.read()
+            data = json.loads(data_str)
+            core_model = CoreModel.import_dict(data)
+
+        assert core_model.sanity_check()
+
+    # def test_sanity_check_IEEE9(self) -> None:
+    #    PATH = pathlib.Path(__file__).parent.parent.resolve()
+
+    #    with open(
+    #        PATH.parent.parent / "tests/models/gdf/IEEE9_gdf.json", "r", encoding="utf-8"
+    #    ) as file:
+    #        data_str = file.read()
+    #        data = json.loads(data_str)
+    #        core_model = CoreModel.import_dict(data)
+
+    #    assert core_model.sanity_check()
 
 
 if __name__ == "__main__":
