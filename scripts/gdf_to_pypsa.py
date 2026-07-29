56b2a36e0df6d7bfb31c96d02baf616bb4664104
Johannes Weinbrecht <johannes.weinbrecht@kit.edu>
2026-07-29 14:29:31 +0200

    cleaned up script file

--- scripts/gdf_to_pypsa.py
+++ scripts/gdf_to_pypsa.py
@@ -17,15 +17,13 @@ PATH = pathlib.Path(__file__).parent.resolve()
 
 
 def main() -> None:
-    model_name = "GredlerAreal4_1"
+    model_name = "IEEE39_gdf"
 
     pd.set_option("future.no_silent_downcasting", True)
 
     start = time.perf_counter()
 
-    with open(
-        PATH.parent / f"tests/models/gdf/IEEE39-flat_gdf.json", "r", encoding="utf-8"
-    ) as file:
+    with open(PATH.parent / f"tests/models/gdf/{model_name}.json", "r", encoding="utf-8") as file:
         data_str = file.read()
         data = json.loads(data_str)
         core_model = CoreModel.import_dict(data)
@@ -35,22 +33,11 @@ def main() -> None:
             core_model, f"{model_name}", log_path=str(PATH.parent / "pypsa.log")
         )
 
-        # Create directory if it does not exist
-        # if not os.path.exists("output/pandapower"):
-        #     os.makedirs("output/pandapower")
-        # converter.write_to_pandapower_json(
-        #     model=pandapower_model, filepath=f"output/pandapower/{model_name}.json"
-        # )
+        if not os.path.exists("output/pypsa"):
+            os.makedirs("output/pypsa")
+        pypsa_model.export_to_netcdf(path=f"output/pypsa/{model_name}.nc")
 
         print(f"conversion took {time.perf_counter() - start:.1f}s")
-
-        pypsa_model.consistency_check()
-        # pypsa_model.lpf()
-        # pypsa_model.optimize()
-        # pypsa_model.fix_optimal_dispatch()
-
-        result = pypsa_model.pf()
-        print(result)
 
 
 if __name__ == "__main__":
